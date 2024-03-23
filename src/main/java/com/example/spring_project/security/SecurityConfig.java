d84c4ba6f8e67426e9e149e2c0d217c11ea08667
Anass boujendar <133109998+AnassSSG@users.noreply.github.com>
2024-03-23 22:13:11 +0000

    Add files via upload

--- src/main/java/com/example/spring_project/security/SecurityConfig.java
+++ src/main/java/com/example/spring_project/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.spring_project.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -12,8 +13,36 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+public class SecurityConfig {
 
+    @Bean
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager(PasswordEncoder passwordEncoder){
+        String encodedPassword = passwordEncoder.encode("1234");
+        System.out.println(encodedPassword);
+        return new InMemoryUserDetailsManager(
+                User.withUsername("user1").password(encodedPassword).roles("USER").build(),
+                User.withUsername("user2").password(encodedPassword).roles("USER").build(),
+                User.withUsername("admin").password(encodedPassword).roles("USER","ADMIN").build()
+        );
+    }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .formLogin(ar->ar.loginPage("/login").permitAll()) //or formLogin(Customizer.withDefaults())
+                .authorizeHttpRequests(ar->ar.requestMatchers("/webjars/** ","/h2-console/**").permitAll())
+                .authorizeHttpRequests(ar->ar.requestMatchers("/patients"). hasRole("USER"))
+                .authorizeHttpRequests(ar->ar.requestMatchers("/delete").hasRole("ADMIN"))
+                .authorizeHttpRequests(ar->ar.requestMatchers("/formPatient").hasRole("ADMIN"))
+                .authorizeHttpRequests(ar->ar.requestMatchers("/editPatient").hasRole("ADMIN"))
+                .authorizeHttpRequests(ar->ar.requestMatchers("/savePatientPatient").hasRole("ADMIN"))
+                .exceptionHandling(ar->ar.accessDeniedPage("/notAuthorized"))
+                .authorizeHttpRequests(ar->ar.anyRequest().authenticated())
+                .build();
+    }
+}
 
+//this code was used with old version of spring boot
+/*
 public class SecurityConfig {
     @Autowired
     private PasswordEncoder passwordEncoder;
@@ -42,3 +71,4 @@ public class SecurityConfig {
         return httpSecurity.build();
         }
 }
+*/
